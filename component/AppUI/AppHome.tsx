e887b4bf5faddf36def387d199431edbf9c12866
ypai@yahoo.com <Y0g1shPa1#>
2021-07-09 08:42:08 -0700

    refactor top bar out

--- component/AppUI/AppHome.tsx
+++ component/AppUI/AppHome.tsx
@@ -17,9 +17,8 @@
 import {Fragment, useState} from 'react';
 import {useIntl} from 'react-intl';
 
-import {Dialog, Menu, Transition} from '@headlessui/react';
+import {Dialog, Transition} from '@headlessui/react';
 import {
-  BellIcon,
   CalendarIcon,
   ChartBarIcon,
   FolderIcon,
@@ -29,7 +28,7 @@ import {
   UsersIcon,
   XIcon,
 } from '@heroicons/react/outline';
-import {SearchIcon} from '@heroicons/react/solid';
+import TopbarForm from './Topbar';
 
 const navigation = [
   {name: 'dashboard', href: '#', icon: HomeIcon, current: true},
@@ -39,19 +38,15 @@ const navigation = [
   {name: 'transactions', href: '#', icon: InboxIcon, current: false},
   {name: 'reports', href: '#', icon: ChartBarIcon, current: false},
 ];
-const userNavigation = [
-  {name: 'yourProfile', href: '#'},
-  {name: 'settings', href: '#'},
-  {name: 'switchAccount', href: '#'},
-  {name: 'signOut', href: '/logout'},
-];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Example() {
+export default function AppHomeForm() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [searchOn, setSearchOn] = useState(false);
+
   const {formatMessage: fmt} = useIntl();
 
   return (
@@ -198,80 +193,7 @@ export default function Example() {
             <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
           </button>
           <div className="flex-1 flex justify-between px-4 md:px-0">
-            <div className="flex-1 flex">
-              <form className="w-full flex md:ml-0" action="#" method="GET">
-                <label htmlFor="search-field" className="sr-only">
-                  Search
-                </label>
-                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                    <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                  </div>
-                  <input
-                    id="search-field"
-                    className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                    placeholder="Search"
-                    type="search"
-                    name="search"
-                  />
-                </div>
-              </form>
-            </div>
-            <div className="ml-4 flex items-center md:ml-6">
-              <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-
-              {/* Profile dropdown */}
-              <Menu as="div" className="ml-3 relative">
-                {({open}) => (
-                  <>
-                    <div>
-                      <Menu.Button className="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      show={open}
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items
-                        static
-                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none"
-                      >
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({active}) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block py-2 px-4 text-sm text-gray-700'
-                                )}
-                              >
-                                {fmt({id: `${item.name}`})}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </>
-                )}
-              </Menu>
-            </div>
+            <TopbarForm searchOn={searchOn} />
           </div>
         </div>
 
